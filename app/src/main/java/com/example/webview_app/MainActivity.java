9b25f80842b41efe2ba8a56f92bc7c0a0465b07c
Henrique Ribeiro Evangelista <henrique.rev@gmail.com>
2020-04-04 12:08:34 -0300

    esqueleto login e pagina MySites criados

--- app/src/main/java/com/example/webview_app/MainActivity.java
+++ app/src/main/java/com/example/webview_app/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.webview_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getUserInfo(View view){
+
+        //get username
+        EditText message = findViewById(R.id.txtUsuario);
+        String user_name = message.getText().toString();
+
+        //get password
+        EditText password = findViewById(R.id.pswLogin);
+        String user_password_str = password.getText().toString();
+        Integer user_password_int = Integer.valueOf(user_password_str);
+
+
+        //push user to my sites page
+        Boolean right_username = true ? user_name.equals("nac01") : false;
+        Boolean right_password = true ? user_password_int == 12345 : false;
+
+        if(right_username && right_password){
+            Intent intent = new Intent(this,MySites.class);
+            intent.putExtra("username",user_name);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Usuário e/ou senha errado(s)!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/webview_app/MySites.java
+++ app/src/main/java/com/example/webview_app/MySites.java
@@ -2,7 +2,9 @@ package com.example.webview_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MySites extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MySites extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sites);
+
+
+        //receive intent from activityMain and get username
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+
+        //set welcome message
+        String welcome_msg = "Seja bem vindo " + username + "!";
+
+        //showing welcome message
+        TextView lbWelcome = findViewById(R.id.txtWelcomeMsg);
+        lbWelcome.setText(welcome_msg);
     }
 }
